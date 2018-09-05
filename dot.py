7692aa659530d59fc684e51591511be1720e4cf6
Sam Peters <samp805@gmail.com>
2018-09-05 18:09:25 -0400

    first commit

---
+++ dot.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+def main(arr):
+    trans = arr.transpose()
+    row1 = trans[0]
+    firstcol = row1.transpose()
+    print(trans @ firstcol)
+
+
+
+if __name__ == '__main__':
+    arr = [1,2,3,4]
+    r = list()
+    r.append(arr)
+    r.append(arr)
+    n = np.array(r)
+    main(n)
